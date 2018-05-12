951553be974cfd8386d9689f0188213f099abb1e
ljh453 <ljh453@postech.ac.kr>
2018-05-12 16:29:36 +0900

    profile initial commit

--- app/src/main/java/kr/ac/postech/jelee/poddk/Profile.java
+++ app/src/main/java/kr/ac/postech/jelee/poddk/Profile.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 
 public class Profile extends Fragment {
@@ -13,7 +14,7 @@ public class Profile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.activity_profile, container, false);
+        View rootView = (RelativeLayout)inflater.inflate(R.layout.activity_profile, container, false);
         return rootView;
     }
 }
