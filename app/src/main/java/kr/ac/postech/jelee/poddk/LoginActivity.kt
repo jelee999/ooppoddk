7f706cdedb759b859bfa8823908bb882c3e9ce09
CHOIJAEYOUNG99 <choijy5847@naver.com>
2018-05-19 17:46:40 +0900

    5_19 CHOI

--- app/src/main/java/kr/ac/postech/jelee/poddk/LoginActivity.kt
+++ app/src/main/java/kr/ac/postech/jelee/poddk/LoginActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
-import kr.ac.postech.jelee.poddk.R.id.registerButton
 
 class LoginActivity : AppCompatActivity() {
 
