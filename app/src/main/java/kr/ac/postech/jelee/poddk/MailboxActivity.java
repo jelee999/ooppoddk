2b36bc221d7a9cbc3e2e17d28853078c70a8f6e9
ljh453 <ljh453@postech.ac.kr>
2018-05-19 18:43:29 +0900

    mailbox

--- app/src/main/java/kr/ac/postech/jelee/poddk/MailboxActivity.java
+++ app/src/main/java/kr/ac/postech/jelee/poddk/MailboxActivity.java
@@ -1,13 +1,47 @@
 package kr.ac.postech.jelee.poddk;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.provider.CallLog;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
-public class MailboxActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class MailboxActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mailbox);
     }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mailbox_menu, menu);
+        if(menu instanceof MenuBuilder)
+        {
+            MenuBuilder m = (MenuBuilder) menu;
+        }
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 }
--- app/src/main/java/kr/ac/postech/jelee/poddk/Profile.java
+++ app/src/main/java/kr/ac/postech/jelee/poddk/Profile.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NotificationBuilderWithBuilderAccessor;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -28,7 +29,7 @@ public class Profile extends Fragment implements View.OnClickListener {
         mailboxBtn = (Button)rootView.findViewById(R.id.mailboxButton);
         managementBtn = (Button)rootView.findViewById(R.id.managementButton);
         developerInfoBtn = (Button)rootView.findViewById(R.id.developerInfoButton);
-        notificationBtn = (Button)rootView.findViewById(R.id.developerInfoButton);
+        notificationBtn = (Button)rootView.findViewById(R.id.NoticeBoardButton);
 
         mailboxBtn.setOnClickListener(this);
         managementBtn.setOnClickListener(this);
@@ -48,14 +49,18 @@ public class Profile extends Fragment implements View.OnClickListener {
         }
         else if (view == managementBtn)
         {
-
+            Intent intent = new Intent(getActivity(), ManagementActivity.class);
+            startActivity(intent);
         }
         else if (view == developerInfoBtn)
         {
-
+            Intent intent = new Intent(getActivity(), DeveloperInfoActivity.class);
+            startActivity(intent);
         }
-        else if (view == notificationBtn) {
-
+        else if (view == notificationBtn)
+        {
+            Intent intent = new Intent(getActivity(), NoticeBoardActivity.class);
+            startActivity(intent);
         }
     }
 }
