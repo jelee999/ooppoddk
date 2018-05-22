6b51afd6e1bb8c1861680c48b6cf8870afc1e982
ljh453 <ljh453@postech.ac.kr>
2018-05-22 19:45:32 +0900

    conflict fixed

--- app/src/main/java/kr/ac/postech/jelee/poddk/Profile.java
+++ app/src/main/java/kr/ac/postech/jelee/poddk/Profile.java
@@ -1,6 +1,8 @@
 package kr.ac.postech.jelee.poddk;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -12,29 +14,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
 
 public class Profile extends Fragment implements View.OnClickListener {
     Button mailboxBtn;
     Button managementBtn;
     Button developerInfoBtn;
-    Button notificationBtn;
+    Button noticeBoardBtn;
+    ImageButton settingBtn;
+    RelativeLayout settingLayout;
+    int v = 0;
 
+    RadioGroup rg;
+    AudioManager am;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = (RelativeLayout) inflater.inflate(R.layout.activity_profile, container, false);
-        mailboxBtn = (Button)rootView.findViewById(R.id.mailboxButton);
-        managementBtn = (Button)rootView.findViewById(R.id.managementButton);
-        developerInfoBtn = (Button)rootView.findViewById(R.id.developerInfoButton);
-        notificationBtn = (Button)rootView.findViewById(R.id.NoticeBoardButton);
+        mailboxBtn = (Button) rootView.findViewById(R.id.mailboxButton);
+        managementBtn = (Button) rootView.findViewById(R.id.managementButton);
+        developerInfoBtn = (Button) rootView.findViewById(R.id.developerInfoButton);
+        noticeBoardBtn = (Button) rootView.findViewById(R.id.NoticeBoardButton);
+        settingBtn = (ImageButton) rootView.findViewById(R.id.setting);
+        settingLayout = (RelativeLayout) rootView.findViewById(R.id.settingLayout);
 
         mailboxBtn.setOnClickListener(this);
         managementBtn.setOnClickListener(this);
         developerInfoBtn.setOnClickListener(this);
-        notificationBtn.setOnClickListener(this);
+        noticeBoardBtn.setOnClickListener(this);
+        settingBtn.setOnClickListener(this);
 
         return rootView;
     }
@@ -42,26 +58,29 @@ public class Profile extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        if (view == mailboxBtn)
-        {
+        if (view == mailboxBtn) {
             Intent intent = new Intent(getActivity(), MailboxActivity.class);
             startActivity(intent);
-        }
-        else if (view == managementBtn)
-        {
+        } else if (view == managementBtn) {
             Intent intent = new Intent(getActivity(), ManagementActivity.class);
             startActivity(intent);
-        }
-        else if (view == developerInfoBtn)
-        {
+        } else if (view == developerInfoBtn) {
             Intent intent = new Intent(getActivity(), DeveloperInfoActivity.class);
             startActivity(intent);
-        }
-        else if (view == notificationBtn)
-        {
+        } else if (view == noticeBoardBtn) {
             Intent intent = new Intent(getActivity(), NoticeBoardActivity.class);
             startActivity(intent);
+        } else if (view == settingBtn) {
+            if (v == 0) v++;
+            else if (v == 1) v--;
+
+            if (v == 1) {
+                settingLayout.setVisibility(VISIBLE);
+            } else {
+                settingLayout.setVisibility(INVISIBLE);
+            }
         }
     }
 }
+
 
